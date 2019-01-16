b1fe05c5faa32753b377a70b64c84910f13d204d
semvs <semvs@ayvansaray.edu.tr>
2019-01-16 09:35:26 +0300

    Eklendi

---
+++ DAL/Urunler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Urunler
+    {
+        public int pId { get; set; }
+        public string  UrunAdi { get; set; }
+        public int Adet { get; set; }
+        prop
+
+
+
+
+    }
+}
