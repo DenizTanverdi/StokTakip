18c693c0605bf5ac909b3bd275e461fb46063bc7
semvs <semvs@ayvansaray.edu.tr>
2019-01-16 10:31:56 +0300

    personel ve urunler eklendi

---
+++ DAL/Personel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Personel
+    {
+        public int calisanId { get; set; }
+        public string CalisanAdi { get; set; }
+        public string CalisanSoyadi { get; set; }
+        public string Görev { get; set; }
+        public string Sehir { get; set; }
+        public string TelNo { get; set; }
+
+    }
+}
