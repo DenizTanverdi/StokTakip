799d255966ccd7836d723feea1e536cf3e327915
mesut <semvs@ayvansaray.edu.tr>
2019-01-16 10:50:12 +0300

    enson

--- DAL/DataAccessLayer.cs
+++ DAL/DataAccessLayer.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-     SqlConnection con;
+
+    class DataAccessLayer
+    {
+        SqlConnection con;
         public DataAccessLayer()
         {
             // con = new SqlConnection(ConfigurationManager.ConnectionStrings["sql"].ConnectionString);
             string adres = "Data Source=.;Initial Catalog=StokTakip;Integrated Security=True";
             con = new SqlConnection(adres);
-        }    
+        }
+    }
 }
---
+++ DAL/Musteriler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Musteriler
+    {
+
+        public int mId { get; set; }
+        public  string MusteriAdi { get; set; }
+        public string  Adres { get; set; }
+        public string  Tel { get; set; }
+
+
+    }
+}
