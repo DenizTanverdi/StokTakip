21adbc6a71b37a7d001d44025ca97eea159c2277
iau <iau@PC-429>
2019-01-23 10:40:01 +0300

    Sipariş Detay Excel Eklendi

--- Personel/satisForm.Designer.cs
+++ Personel/satisForm.Designer.cs
@@ -33,6 +33,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.saveFilesatış = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -52,6 +53,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Location = new System.Drawing.Point(12, 365);
             this.groupBox1.Name = "groupBox1";
@@ -80,6 +82,16 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // satisForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -105,5 +117,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.SaveFileDialog saveFilesatış;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Personel/satisForm.cs
+++ Personel/satisForm.cs
@@ -1,5 +1,6 @@
 ﻿using BLL;
 using DAL;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Personel
 {
@@ -54,6 +56,43 @@ List<Satis> satis = new List<Satis>();
                 tw.Close();
                 MessageBox.Show("liste kaydedildi.");
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel.Application exceldosya = new Excel.Application();
+
+            exceldosya.Visible = true;
+            object Missing = Type.Missing;
+            Workbook calismakitabi = exceldosya.Workbooks.Add(Missing);
+            Worksheet sheet1 = (Worksheet)calismakitabi.Sheets[1];
+            int sutun = 1;
+            int satir = 1;
+
+
+
+            for (int j = 0; j < dataGridView1.ColumnCount; j++)
+            {
+
+                Range myrange = (Range)sheet1.Cells[satir, sutun + j];
+                myrange.Value2 = dataGridView1.Columns[j].HeaderText;
+
+            }
+
+            satir++;
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    Range myrange = (Range)sheet1.Cells[satir + i, sutun + j];
+                    myrange.Value2 = dataGridView1[j, i].Value == null ? "" : dataGridView1[j, i].Value;
+                    myrange.Select();
+                }
+            }
+
+
+
         }
     }
 }
