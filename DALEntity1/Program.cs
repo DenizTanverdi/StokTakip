0a42b4e8f83444ab37d18e15397c017b9e429abd
semvs <semvs@ayvansaray.edu.tr>
2019-01-24 09:56:17 +0300

    Entity Join

--- DALEntity1/Program.cs
+++ DALEntity1/Program.cs
@@ -11,13 +11,20 @@ namespace DALEntity1
         static void Main(string[] args)
         {
             StokTakipDEntities stk = new StokTakipDEntities();
-            Users users = new Users();
-            users.email = "dd@gg.com";
-            users.pasword = "123123";
-            users.isLogin = true;
-            stk.Users.Add(users);
-            stk.SaveChanges();
-            Console.WriteLine("Hello World!");
+            #region Users Ekleme
+            //Users users = new Users();
+            //users.email = "dd@gg.com";
+            //users.pasword = "123123";
+            //users.isLogin = true;
+            //stk.Users.Add(users);
+            //stk.SaveChanges();
+            #endregion
+            var res = from d in stk.Detay join s in stk.Siparis on d.sId equals s.sId join m in stk.Musteri on s.mId equals m.mId join u in stk.Urunler on d.pId equals u.pId select new { m.MusteriAdi,u.UrunAdi,d.miktar,d.fiyat,s.SiparisTarih };
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.MusteriAdi+" "+item.UrunAdi+" "+item.miktar+" "+item.fiyat+" "+item.SiparisTarih);
+            }
+           // Console.WriteLine("Hello World!");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
